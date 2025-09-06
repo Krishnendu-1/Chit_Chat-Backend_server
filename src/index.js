4db5d5cccc9c6efc241a13b0fef67bbd41ca531a
Krishnendu Roy <roykrishnenduroy01@gmail.com>
2025-09-07 02:43:27 +0530

    mongodb connection error solved

--- src/index.js
+++ src/index.js
@@ -32,10 +32,7 @@ app.use(cors(
     }
 ));
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
     console.error('MongoDB connection error:', err);
